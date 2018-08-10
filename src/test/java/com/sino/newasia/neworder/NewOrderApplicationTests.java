7990c5f23a65db7dba84e3955133617554682662
robertdenilo <b47849@hotmail.com>
2018-08-10 16:03:06 -0400

    init

--- src/test/java/com/sino/newasia/neworder/NewOrderApplicationTests.java
+++ src/test/java/com/sino/newasia/neworder/NewOrderApplicationTests.java
@@ -1,16 +1,35 @@
 package com.sino.newasia.neworder;
 
+import com.sino.newasia.neworder.Controller.HelloIJ;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class NewOrderApplicationTests {
+    private MockMvc mvc;
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new HelloIJ()).build();
+    }
+
 
     @Test
     public void contextLoads() {
+    }
+
+
+    @Test
+    public void getHello()
+    {
+
+
     }
 
 }
