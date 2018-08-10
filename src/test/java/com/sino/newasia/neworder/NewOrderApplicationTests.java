03935809cd7ce5433d41cf893c5ffe1665405857
robertdenilo <b47849@hotmail.com>
2018-08-10 17:02:14 -0400

    init

--- src/test/java/com/sino/newasia/neworder/NewOrderApplicationTests.java
+++ src/test/java/com/sino/newasia/neworder/NewOrderApplicationTests.java
@@ -1,13 +1,20 @@
 package com.sino.newasia.neworder;
 
 import com.sino.newasia.neworder.Controller.HelloIJ;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -28,6 +35,18 @@ public class NewOrderApplicationTests {
     @Test
     public void getHello()
     {
+        try {
+            mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andDo(MockMvcResultHandlers.print())
+                    .andReturn();
+
+            System.out.println("ming ming");
+            assertEquals(24+9,33);
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
 
 
     }
