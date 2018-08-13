ac18b087e9018f32b50383c78b98b6e501fadb50
robertdenilo <b47849@hotmail.com>
2018-08-13 00:08:59 -0500

    20180812-1

--- src/main/java/com/sino/newasia/neworder/Controller/HelloIJ.java
+++ src/main/java/com/sino/newasia/neworder/Controller/HelloIJ.java
@@ -1,10 +1,21 @@
 package com.sino.newasia.neworder.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
+
+
 @RestController
+@RequestMapping("/test")
 //@EnableAutoConfiguration
 public class HelloIJ {
 
@@ -13,5 +24,48 @@ public class HelloIJ {
 
         return String.format("hello xxxx  %s", Name);
     }
+   @RequestMapping("/login")
+    private String Login(@RequestParam String aa){
+        return  "";
+   }
 
+   @Autowired
+   private JdbcTemplate jdbcTemplate;
+   @RequestMapping("/mydb/getData")         //use jdbcTemplate
+   private List<Map<String, Object>>  DBTest(){
+       String sql = "select * from t_user";
+       List<Map<String, Object>> list =  jdbcTemplate.queryForList(sql);
+       for (Map<String, Object> map : list) {
+           Set<Entry<String, Object>> entries = map.entrySet( );
+           if(entries != null) {
+               Iterator<Entry<String, Object>> iterator = entries.iterator( );
+               while(iterator.hasNext( )) {
+                   Entry<String, Object> entry =(Entry<String, Object>) iterator.next( );
+                   Object key = entry.getKey( );
+                   Object value = entry.getValue();
+                   System.out.println(key+":"+value);
+               }
+           }
+       }
+       return list;
+   }
+
+    @RequestMapping("/mydb/getData/{id}")
+    public Map<String,Object> DBTest_userid(@PathVariable Integer id){
+        Map<String,Object> map = null;
+        List<Map<String, Object>> list = DBTest();
+        for (Map<String, Object> dbmap : list) {
+            Set<String> set = dbmap.keySet();
+            for (String key : set) {
+                if(key.equals("id")){
+                    if(dbmap.get(key).equals(id)){
+                        map = dbmap;
+                    }
+                }
+            }
+        }
+        if(map==null)
+            map = list.get(0);
+        return map;
+    }
 }
--- src/main/java/com/sino/newasia/neworder/LoginController/LoginController.java
+++ src/main/java/com/sino/newasia/neworder/LoginController/LoginController.java
@@ -1,7 +1,5 @@
 package com.sino.newasia.neworder.LoginController;
 
-import com.sino.newasia.neworder.LoginService.LoginService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,18 +7,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
-    @Autowired
-    private LoginService ls;
+    //@Autowired
+    //private LoginService ls;
 
     public LoginController() {
     }
 
     @RequestMapping(value = "/asia/login")
-       public String login(@RequestParam("username") String userName, Model model){
-           model.addAttribute("userName", userName);
+    public String login(@RequestParam("username") String userName, Model model){
+/*           model.addAttribute("userName", userName);
            String loginStatus = ls.Login(userName);
-           model.addAttribute("loginStatus", loginStatus);
+           model.addAttribute("loginStatus", loginStatus);*/
            return "loginResult";
-       }
+    }
 
 }
